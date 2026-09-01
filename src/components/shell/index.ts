abd14cf3d960002104ad991698c16aec325863ad
Srivatsa <srivatsagorti@gmail.com>
2026-09-01 22:28:20 +0530

    fix: add BakaSurPresence to shell barrel export
    Layout.tsx imports BakaSurPresence from '../shell' but the barrel
    at src/components/shell/index.ts did not re-export it. This is the
    sole cause of the Pages build failure (TS2305).

--- src/components/shell/index.ts
+++ src/components/shell/index.ts
@@ -1,5 +1,7 @@
 export { ContextBar } from './ContextBar';
 export { BakaSurRail } from './BakaSurRail';
+export { BakaSurPresence } from './BakaSurPresence';
+export { useBakaSurBusyReporter, useBakaSurChatOpen, useBakaSurPresenceSlot } from './bakaSurPresenceContext';
 export { CabinetSurface, AttractState, EmptyState } from './ArcadePrimitives';
 export { GlassContainer, GlassSurface } from './GlassContainer';
 export { SyncStatus } from './SyncStatus';
